81446b2bbe0efa75616162a5aa234abf726e7893
Sanskar Dwivedi <37977161+mrinalsanskar@users.noreply.github.com>
2021-02-17 13:09:27 +0530

    Update script.js
    added jquery for dark mode

--- script.js
+++ script.js
@@ -70,3 +70,14 @@ $(document).ready(function() {
         }
     });
 });
+
+//for dark mode
+$(".inner-switch").on("click", function() {
+    if ($("body").hasClass("dark")) {
+        $("body").removeClass("dark");
+        $(".inner-switch").text("OFF");
+    } else {
+        $("body").addClass("dark");
+        $(".inner-switch").text("ON");
+    }
+});
